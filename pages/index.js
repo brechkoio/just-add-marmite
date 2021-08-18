cd3fd9301b3eaa3f54a979f0cac7dbd2e79870f6
brechkoio <brechkoio@gmail.com>
2021-08-18 14:25:42 +0300

    add paths and rich text content

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,31 @@
-import { createClient } from 'contentful';
-import RecipeCard from '../components/RecipeCard';
+import { createClient } from 'contentful'
+import RecipeCard from '../components/RecipeCard'
 
 export async function getStaticProps() {
 
   const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
     accessToken: process.env.CONTENTFUL_ACCESS_KEY,
-  });
+  })
 
-  const res = await client.getEntries({ content_type: 'recipe'});
+  const res = await client.getEntries({ content_type: "recipe" })
 
   return {
     props: {
-      recipes: res.items
+      recipes: res.items,
     }
   }
-
 }
 
 export default function Recipes({ recipes }) {
+  console.log(recipes)
+
   return (
-    
     <div className="recipe-list">
       {recipes.map(recipe => (
-        <RecipeCard key={recipe.sys.id} recipe={recipe}/>
+        <RecipeCard key={recipe.sys.id} recipe={recipe} />
       ))}
+
       <style jsx>{`
         .recipe-list {
           display: grid;
