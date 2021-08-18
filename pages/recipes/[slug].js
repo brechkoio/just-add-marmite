a135ea6c4dd4c60dca47f2ccf7e90b50174dbc17
brechkoio <brechkoio@gmail.com>
2021-08-18 15:48:45 +0300

    redirect

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from 'contentful';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import Image from 'next/image';
+import Skeleton from '../../components/Skeleton';
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -20,7 +21,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false
+    fallback: true
   }
 }
 
@@ -30,6 +31,10 @@ export const getStaticProps = async ({ params }) => {
     'fields.slug': params.slug
   })
 
+  if (!items.length) {
+    
+  }
+
   return {
     props: { recipe: items[0] },
     revalidate: 1
@@ -38,6 +43,8 @@ export const getStaticProps = async ({ params }) => {
 }
 
 export default function RecipeDetails({ recipe }) {
+  if (!recipe) return <Skeleton />
+
   const { featuredImage, title, cookingTime, ingredients, method} = recipe.fields;
   
   return (
